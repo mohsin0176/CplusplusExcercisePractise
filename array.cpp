070024a18e7c2dc232dbf934d9807c1abd7ed4f0
mohsin0176 <mohsin2010ju@gmail.com>
2021-02-08 22:09:53 +0600

    Code Created

--- array.cpp
+++ array.cpp
@@ -2,24 +2,47 @@
 #include<iostream>
 using namespace std;
 
-void shift_right(int *A, int size, int p)
-
-{
-         int i;
-         int tmp[size];
-         p = p%size;
-         for(i=0;i<size;i++)
-         {
-        tmp[ i+p>=0? (i+p)%size : size+i+p ] = A[i];
-         }
-         for(i=0;i<size;i++)
-         {
-        A[i] = tmp[i];
-         }
-}
 
 int main()
 {
-    shift_right(int arr[], 5, 2);
+    int i;
+    int j;
+    int s;
+    int sh;
+    cout<<"Enter Size of Array:"<<endl;
+    cin>>s;
+    cout<<"Enter Number Of Position to be Shifted:"<<endl;
+    cin>>sh;
+    int arr[s];
+    int tmp[s];
+    cout<<"Enter Elements of Array:"<<endl;
+    for(i=0;i<s;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"Entered Array:"<<endl;
+    for(i=0;i<s;i++)
+    {
+        cout<<arr[i]<<"\t";
+    }
+    cout<<endl;
+    //cout<<"Shifting Start:"<<endl;
+    for(i=(s-sh);i<s;i++)
+    {
+        tmp[i-3]=arr[i];
+        if(i=(s-1))
+        {
+            for(j=0;j<=sh;j++)
+            {
+                tmp[j+2]=arr[j];
+            }
+        }
+
+    }
+    cout<<"Shifted Array:"<<endl;
+    for(i=0;i<s;i++)
+    {
+        cout<<tmp[i]<<"\t";
+    }
     return 0;
 }
