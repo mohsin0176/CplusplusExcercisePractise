911fcd6b9cee7b2f39411f2430f0e7cc64b82abc
mohsin0176 <mohsin2010ju@gmail.com>
2021-02-09 23:38:35 +0600

    Code Created

---
+++ function-return-refe-val.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+int add(int n1,int n2)
+{
+    int sum=n1+n2;
+    int s=sum;
+    return s;
+}
+
+int main()
+{
+    int a=10;
+    int b=20;
+    int num1=a;
+    int num2=b;
+    cout<<add(num1,num2);
+    return 0;
+}
