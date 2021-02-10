9ba094ef7f0af67f1e49a523f0e22af96b93a284
mohsin0176 <mohsin2010ju@gmail.com>
2021-02-10 22:45:00 +0600

    Code Created

--- pointer-array-no-loop.cpp
+++ pointer-array-no-loop.cpp
@@ -4,14 +4,10 @@ using namespace std;
 
 int main()
 {
-    int *ptr;
+
     int arr[10]={10,20,30,40,50,60,70,80,90,100};
-    ptr=arr;
+    int *ptr=arr;
     int i;
-    for(i=0;i<9;i++)
-    {
-        cin>>*(ptr+i);
-    }
       for(i=0;i<9;i++)
     {
         cout<<*(ptr+i)<<"\t";
