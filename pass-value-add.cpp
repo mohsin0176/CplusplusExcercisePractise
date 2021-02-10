edf36552beb4d3b430980da0d15fd4d9fce0f725
mohsin0176 <mohsin2010ju@gmail.com>
2021-02-10 22:38:25 +0600

    Code Created

---
+++ pass-value-add.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+int fun(int &num);
+int fun(int &num)
+{
+    cout<<num;
+}
+int main()
+{
+    int a=10;
+    fun(a);
+    return 0;
+}
